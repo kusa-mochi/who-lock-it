2fccf666785b048a3c677f4b8bcf5fba2cb24835
kusa-mochi <whoatemyapplepie@gmail.com>
2019-07-08 22:46:54 +0900

    ファイルを開いているプロセスの名前の表示ができるようになった。

---
+++ WhoUseThis/WhoUseThis/Views/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using ProcessLibrary;
+
+namespace WhoUseThis.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string filePath = @"C:\src\github\who-use-this\README.md";
+            List<Process> processList = Win32Processes.GetProcessesLockingFile(filePath);
+            foreach(Process p in processList)
+            {
+                MessageBox.Show(p.ProcessName);
+            }
+        }
+    }
+}
