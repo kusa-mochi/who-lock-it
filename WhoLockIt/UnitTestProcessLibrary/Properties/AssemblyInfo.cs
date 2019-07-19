0c554d976b875d596937efec84d87b091aab755a
kusa-mochi <whoatemyapplepie@gmail.com>
2019-07-19 21:05:52 +0900

    add unittest project for ProcessLibrary.

---
+++ WhoLockIt/UnitTestProcessLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProcessLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProcessLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7fc9d8c7-5b86-48bc-a2bf-ca4c72bb3115")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WhoLockIt/UnitTestProcessLibrary/UnitTestWin32Processes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ProcessLibrary;
+
+namespace UnitTestProcessLibrary
+{
+    [TestClass]
+    public class UnitTestWin32Processes
+    {
+        private string _openedFilePath = @"c:\tmp\mytext.txt";
+        private string _openedDirectoryPath = @"c:\tmp\myfolder\";
+
+        /// <summary>
+        /// このテストを実行する前に，
+        /// c:\tmp\mytext.txt
+        /// を何かしらのプログラムで開かれた状態にしておくこと。
+        /// </summary>
+        [TestMethod]
+        public void TestOpenedFile()
+        {
+            List<Process> processes = Win32Processes.GetProcessesLockingFile(_openedFilePath);
+            Assert.IsNotNull(processes);
+            Assert.AreNotEqual(0, processes.Count);
+        }
+
+        /// <summary>
+        /// このテストを実行する前に，
+        /// c:\tmp\myfolder\
+        /// をコマンドプロンプトで開かれた状態にしておくこと。
+        /// </summary>
+        [TestMethod]
+        public void TestOpenedDirectory()
+        {
+            List<Process> processes = Win32Processes.GetProcessesLockingFile(_openedDirectoryPath);
+            Assert.IsNotNull(processes);
+            Assert.AreNotEqual(0, processes.Count);
+        }
+    }
+}
