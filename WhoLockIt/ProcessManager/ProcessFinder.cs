adece7275a985dfc68203db84bcd3170ef700032
kusa-mochi <whoatemyapplepie@gmail.com>
2019-07-19 21:44:06 +0900

    add a library project to find which program is locking a folder / file (instead of ProcessLibrary project).

---
+++ WhoLockIt/ProcessManager/ProcessFinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management.Automation; // Win10 SDK is required.
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessManager
+{
+    public static class ProcessFinder
+    {
+        private static string _command = ""
+            + "Function FindLockingProcess"
+            + "{"
+                + "Param"
+                + "("
+                    + "[Parameter(Mandatory=$true)]"
+                    + "[String] $FileOrFolderPath"
+                + ")"
+                + "IF((Test-Path -Path $FileOrFolderPath) -eq $false) {"
+                    + "Write-Warning \"File or directory does not exist.\""
+                + "}"
+                + "Else {"
+                    + "$LockingProcess = CMD /C \"openfiles /query /fo table | find /I \"\"$FileOrFolderPath\"\"\""
+                    + "Write-Host $LockingProcess"
+                + "}"
+            + "}";
+
+        public static List<string> FindLockingProcessNames(string fileOrFolderPath)
+        {
+            List<string> output = new List<string>();
+
+            using (var invoker = new RunspaceInvoke())
+            {
+                var result = invoker.Invoke(_command, new[] { fileOrFolderPath });
+                foreach (var r in result)
+                {
+                }
+            }
+
+            return output;
+        }
+    }
+}
